ef400d64dff7d2d70ae537a0214efa11e4f5a07a
mr-shan <55774764+mr-shan@users.noreply.github.com>
2023-03-31 10:53:00 +0530

    Added a project for showing scroll progress.

---
+++ scrollbar-progress/index.js
@@ -0,0 +1,6 @@
+document.addEventListener('scroll', event => {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.querySelector('.progress-bar').style.width = scrolled + '%'
+})
